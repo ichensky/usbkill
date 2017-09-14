163d16223ca419c8d59306836d381b2b35be7b86
2T242S%CSE$Bp%.@YЂ%y..‡#E.u.‰C.—Qђќуќу-.CЩ«ф8ЛD8».C.OЯЩEDод;;ѕВ.. яЩњt)8.0=яЯШ.2T242S%CSE$Bp%.@YЂ%y..‡#E.u.‰C.—Qђќуќу-.CЩ«ф8ЛD8».C.OЯЩEDод;;ѕВ.. яЩњt)8.0=яЯШ.2T242S%CSE$Bp%.@YЂ%y..‡#E.u.‰C.—Qђќуќу-.CЩ«ф8ЛD8».C.OЯЩEDод;;ѕВ.. яЩњt)8.0=яЯШ.2T242S%CSE$Bp% <IChensky@live.com>
2017-09-15 00:05:09 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,5 @@
 /* List of all USB devices you want whitelisted (can be inserted into pc) */
 static const struct usb_device_id whitelist_add_devices[] = {
-	{ USB_DEVICE(0x093a, 0x2510) },
 	{ USB_DEVICE(0x0000, 0x0000) },
 };
 /* List of all USB devices you want whitelisted (can be removed from pc) */
